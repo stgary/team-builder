6cea1d08284ac7050d6eec8850060a84eff2f274
Stephen M. Gary <stgary0@gmail.com>
2020-05-19 16:45:11 -0400

    setup state, handlers, props in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { v4 as uuid } from 'uuid';
+import React, { useState } from 'react'
+// import { v4 as uuid } from 'uuid'
 
 import Form from './Form';
 import List from './List';
 
 const initialMembersList = [
   {
-    id: uuid(),
+    id: Date.now(),
     username: 'Michael',
     email: 'michael@michael.com',
     role: 'Student'
@@ -20,10 +20,42 @@ const initialFormValues = {
 }
 
 export default function App() {
+
+  const [members, setMembers] = useState(initialMembersList);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const onSubmit = e => {
+    e.preventDefault()
+    if (
+      !formValues.username.trim() ||
+      !formValues.email.trim() ||
+      !formValues.role.trim()
+      ) { return }
+    const newMember = { ...formValues, id: Date.now() }
+    setMembers([ newMember, ...members ])
+    setFormValues(initialFormValues)
+  }
+
+  const onInputChange = e => {
+    const { name } = e.target
+    const { value } = e.target
+    setFormValues({ ...formValues, [ name ]: value })
+  }
+
   return (
     <div>
-      <Form />
-      <List />
+      <Form
+        value={formValues}
+        onInputChange={onInputChange}
+        onSubmit={onSubmit}
+      />
+      {
+        members.map(member => {
+          return (
+            <List key={member.id} details={member} />
+          )
+        })
+      }
     </div>
   );
 }
