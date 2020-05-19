0cb74854c3c63defe84eee285602b49803535e96
Stephen M. Gary <stgary0@gmail.com>
2020-05-19 17:02:30 -0400

    setup List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 export default function List(props) {
+  const { details } = props
+
+  if (!details) { return <h3>Working on fetching member details...</h3> }
+
   return (
-    <div>
-      
+    <div className='list-container'>
+      <h2>{ details.username }</h2>
+      <p>Email: { details.email }</p>
+      <p>Role: { details.role }</p>
     </div>
   );
 }
